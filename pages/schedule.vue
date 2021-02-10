598a45c6b150d9fa7138bcef39f34bbd3f618d66
Peter Varsanyi <pvarsany@amazon.com>
2021-02-09 20:23:11 -0800

    wip

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -11,18 +11,18 @@
               :options="options"
               :series="series"
               height="100%"
-              width="80%"
+              width="100%"
             ></apexchart>
           </client-only>
           {{ series }}
         </v-container>
-        <v-card v-for="range in ranges" :key="range.start.toISOString()">
+        <v-card v-for="range in ranges" :key="range.start.toISO()">
           <v-card-text
-            >{{ range.start.hours() }}:{{ range.start.minutes() }} -
-            {{ range.end.hour() }}:{{ range.end.minutes() }}</v-card-text
+            >{{ range.start.hour }}:{{ range.start.minute }} -
+            {{ range.end.hour }}:{{ range.end.minute }}</v-card-text
           >
         </v-card>
-        {{ ranges }}
+        Ranges: {{ ranges }}
         <v-row>
           <v-col>
             <v-card>
@@ -30,6 +30,8 @@
               <v-card-text>
                 <v-text-field v-model="start" label="start"></v-text-field>
                 <v-text-field v-model="end" label="end"></v-text-field>
+                <v-text-field v-model="tz" label="Timezone" />
+                <v-text-field v-model="day" label="Day"></v-text-field>
                 <v-btn @click="addRange()"></v-btn>
               </v-card-text>
             </v-card>
@@ -42,17 +44,20 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-// import moment, { Moment } from 'moment'
-import * as moment from 'moment'
+import { DateTime } from 'luxon'
+import { ApexOptions } from 'apexcharts'
 
 class Interval {
-  start: moment.Moment
-  end: moment.Moment
+  start: DateTime
+  end: DateTime
   type: string
-  constructor(start: moment.Moment, end: moment.Moment, type: string) {
+  day: string
+
+  constructor(start: DateTime, end: DateTime, type: string, day: string) {
     this.start = start
     this.end = end
     this.type = type
+    this.day = day
   }
 }
 
@@ -62,7 +67,10 @@ export default class Schedule extends Vue {
   start: string = '08:00'
   end: string = '12:00'
 
-  options = {
+  day: string = 'mon'
+  tz: string = 'PST'
+
+  options: ApexOptions = {
     chart: {
       type: 'rangeBar',
     },
@@ -72,51 +80,115 @@ export default class Schedule extends Vue {
         barHeight: '50%',
       },
     },
+    annotations: {
+      xaxis: [
+        // {
+        //   x: DateTime.local(2021, 1, 1, 0, 0).toJSDate().getTime(),
+        //   borderColor: '#775DD0',
+        //   style: {
+        //     color: '#fff',
+        //   },
+        //
+        //   label: {
+        //     orientation: 'horizontal',
+        //     text: 'monday',
+        //   },
+        // },
+        // {
+        //   x: DateTime.local(2021, 1, 10).toJSDate().getTime(),
+        //   label: {
+        //     orientation: 'horizontal',
+        //     text: 'Sunday',
+        //   },
+        // },
+      ],
+    },
     dataLabels: {
-      enabled: true,
+      enabled: false,
       formatter: (vals, opts) => {
-        console.log(vals)
         const label = opts.w.globals.labels[opts.dataPointIndex]
-        console.log(label)
         return label
       },
     },
     xaxis: {
+      tickAmount: 24,
       type: 'datetime',
-      min: new Date('2019-03-01').getTime(),
-      max: new Date('2019-03-08').getTime(),
+      labels: {
+        // offsetX: -35,
+        formatter: (value, timestamp, opts) => {
+          const x = DateTime.fromJSDate(new Date(value))
+          return x.toFormat('HH:mm')
+          // return opts.dateFormatter(new Date(timestamp)).format('dd MMM')
+        },
+      },
+      min: DateTime.local(2021, 1, 1, 0, 0).toJSDate().getTime(),
+      max: DateTime.local(2021, 1, 2, 0, 0).toJSDate().getTime(),
     },
   }
 
   series = [
     {
-      data: [
-        {
-          x: 'Gwelican',
-          y: [
-            new Date('2019-03-02').getTime(),
-            new Date('2019-03-04').getTime(),
-          ],
-        },
-      ],
+      data: [],
     },
   ]
 
-  addRange() {
-    this.ranges.push(
-      new Interval(
-        moment(this.start, 'hh:mm'),
-        moment(this.end, 'hh:mm'),
-        'free'
+  weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+  mounted() {
+    for (let i = 0; i < 7; i++) {
+      this.ranges.push(
+        new Interval(
+          DateTime.local(2021, 1, 1, 8, 0),
+          DateTime.local(2021, 1, 1, 10, 0),
+          'free',
+          this.weekdays[i]
+        )
       )
-    )
+    }
+    // this.ranges.push(
+    //   new Interval(
+    //     DateTime.local(2021, 1, 4, 8, 0),
+    //     DateTime.local(2021, 1, 4, 10, 0),
+    //     'free',
+    //     'mon'
+    //   )
+    // )
+    // this.ranges.push(
+    //   new Interval(
+    //     DateTime.local(2021, 1, 4, 8, 0),
+    //     DateTime.local(2021, 1, 4, 10, 0),
+    //     'free',
+    //     'tue'
+    //   )
+    // )
+    this.updateChart()
+  }
+
+  addRange() {
+    const startDate = DateTime.fromFormat(this.start, 'hh:mm').set({
+      day: 4,
+      year: 2021,
+      month: 1,
+    })
+    const endDate = DateTime.fromFormat(this.end, 'hh:mm').set({
+      day: 4,
+      year: 2021,
+      month: 1,
+    })
+
+    this.ranges.push(new Interval(startDate, endDate, 'free'))
+    this.updateChart()
+  }
+
+  private updateChart() {
     const x = this.series.slice()
+    x[0].data = []
 
     for (const range: Interval of this.ranges) {
       console.log(range)
       x[0].data.push({
-        x: 'Gwelican',
-        y: [range.start.toDate().getTime(), range.end.toDate().getTime()],
+        x: range.day,
+        y: [range.start.toJSDate().getTime(), range.end.toJSDate().getTime()],
       })
     }
 
