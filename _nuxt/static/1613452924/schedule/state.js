ee3ebfe2bdd0f5cde5786361f514b6fd40c842cd
gwelican <gwelican@users.noreply.github.com>
2021-02-17 08:26:28 +0000

    deploy: f364a79b3db340eb839c9145f792764baf9d2736

--- _nuxt/static/1613452924/schedule/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fwow-schedule\u002F_nuxt\u002Fstatic\u002F1613452924",layout:"default",error:null,state:{availability:{series:[]}},serverRendered:true,routePath:"\u002Fschedule",config:{},apollo:{defaultClient:Object.create(null)}};
